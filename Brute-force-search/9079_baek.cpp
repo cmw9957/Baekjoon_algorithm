64522f1c7d9a5810c8071ae10a3c27d32143c599
cmwmom <68580121+cmwmom@users.noreply.github.com>
2021-07-21 22:41:05 +0900

    boj

---
+++ Brute-force-search/9079_baek.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string c[3];
+
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	
+	while(t--){
+		for(int i = 0;i < 3;i++)
+			cin >> c[i];
+		
+		
+	}
+	return 0;
+}
